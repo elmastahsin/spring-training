eebb75b7a64c8044b59826c9a706cac6f719c794
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-05-22 23:18:09 +0300

    spring data

--- spring-12-Data-Relationship/src/main/java/com/company/Spring12DataRelationshipApplication.java
+++ spring-12-Data-Relationship/src/main/java/com/company/Spring12DataRelationshipApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataApplication {
+public class Spring12DataRelationshipApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DataApplication.class, args);
-
+        SpringApplication.run(Spring12DataRelationshipApplication.class, args);
     }
+
 }
---
+++ spring-12-Data-Relationship/src/main/java/com/company/entity/Payment.java
@@ -0,0 +1,32 @@
+package com.company.entity;
+
+import com.company.enums.Status;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "DATE")
+    private LocalDate createdDate;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private Status paymentStatus;
+
+    public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
+        this.createdDate = createdDate;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
+}
