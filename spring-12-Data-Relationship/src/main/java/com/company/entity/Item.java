958d1e866d501a55b7c83150cdd6c17a61d694a3
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-05-23 13:28:35 +0300

    spring data

--- spring-12-Data-Relationship/src/main/java/com/company/entity/Item.java
+++ spring-12-Data-Relationship/src/main/java/com/company/entity/Item.java
@@ -17,4 +17,8 @@ public class Item {
     private String name;
     private String code;
 
+    public Item(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
 }
