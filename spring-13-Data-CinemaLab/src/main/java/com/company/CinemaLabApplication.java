a8df93c34ad635bb29ec14a1d42c64948552f56f
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-05-23 20:09:26 +0300

    spring data

--- spring-13-Data-CinemaLab/src/main/java/com/company/CinemaLabApplication.java
+++ spring-13-Data-CinemaLab/src/main/java/com/company/CinemaLabApplication.java
@@ -1,7 +1,12 @@
 package com.company;
 
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CinemaLabApplication {
@@ -10,4 +15,8 @@ public class CinemaLabApplication {
         SpringApplication.run(CinemaLabApplication.class, args);
     }
 
+    @Bean
+    public MigrateResult migrateResult(DataSource dataSource) {
+        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+    }
 }
