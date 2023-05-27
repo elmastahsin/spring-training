61ff5fe8c6daa18c3e82072df78274959ee2379a
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-05-27 20:10:18 +0300

    spring data

---
+++ spring-14-Data-Queries/src/main/java/com/company/entity/Department.java
@@ -0,0 +1,20 @@
+package com.company.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "departments")
+@NoArgsConstructor
+@Data
+public class Department {
+
+    @Id
+    private String department;
+    private String division;
+}
