b15184ef54fc5d2b561fa3dfe6b2dc760c55ee57
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-05-28 15:38:19 +0300

    spring data queries

--- spring-14-Data-Queries/src/main/java/com/company/repository/CourseRepository.java
+++ spring-14-Data-Queries/src/main/java/com/company/repository/CourseRepository.java
@@ -4,6 +4,7 @@ import com.company.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
@@ -22,5 +23,7 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     //find all the courses taht start with the given course name string
     List<Course> findByNameStartingWith(String name);
 
+    // Find all courses by category and return a stream
+    Stream<Course> streamAllByCategory(String category);
 
 }
