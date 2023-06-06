76112449e0c5552dd33761df5d1eeb75123b273e
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-06-07 00:31:59 +0300

    spring data queries cinema lab

--- spring-13-Data-CinemaLab/src/main/java/com/company/repository/AccountRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/company/repository/AccountRepository.java
@@ -16,21 +16,27 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     //Write a derived query to list all accounts with a specific country or state
     List<Account> findAllByCountryOrState(String country, String state);
+
     //Write a derived query to list all accounts with age lower than or equal to a specific value
+    List<Account> findAllByAgeLessThanEqual(Integer age);
 
     //Write a derived query to list all accounts with a specific role
+    List<Account> finAllByRole(UserRole role);
 
     //Write a derived query to list all accounts between a range of ages
+    List<Account> findAllByAgeBetween(Integer age1, Integer age2);
 
     //Write a derived query to list all accounts where the beginning of the address contains the keyword
-
-
+    List<Account> findAllByAddressStartingWith(String address);
     //Write a derived query to sort the list of accounts with age
-
+    List <Account> findAllByOrderByAgeDesc();
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns all accounts
+
+    @Query("SELECT a FROM Account a")
+    List<Account> fetchAllJPQL();
 
 
     //Write a JPQL query to list all admin accounts
@@ -42,10 +48,9 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all accounts with an age lower than a specific value
-                                         // Can be used with @Param("age") too
+    // Can be used with @Param("age") too
 
     //Write a native query to read all accounts that a specific value can be containable in the name, address, country, state, city
-
 
 
     //Write a native query to read all accounts with an age higher than a specific value
