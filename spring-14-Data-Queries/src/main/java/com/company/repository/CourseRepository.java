ef04f7c0411d09df3dc3f95077a5d8e58df2833d
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-06-06 23:54:47 +0300

    spring data queries cinema lab

--- spring-14-Data-Queries/src/main/java/com/company/repository/CourseRepository.java
+++ spring-14-Data-Queries/src/main/java/com/company/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package com.company.repository;
 
 import com.company.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.stream.Stream;
@@ -25,5 +27,8 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
 
     // Find all courses by category and return a stream
     Stream<Course> streamAllByCategory(String category);
+
+    @Query("SELECT c FROM Course c WHERE c.category = :category AND c.rating > :rating")
+    List<Course> retrieveAllByCategoryAndRatingGreater(@Param("category") String category, @Param("rating") double rating);
 
 }
