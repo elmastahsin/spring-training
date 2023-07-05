da4594ed7170991f0de591d5e8765757a8d8e569
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-07-05 12:27:52 +0300

    spring rest

--- spring-15-REST/src/main/java/com/company/controller/CourseController.java
+++ spring-15-REST/src/main/java/com/company/controller/CourseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController //@Controller + @ResponseBody
-@RequestMapping("/courses/api/v1")
+@RequestMapping("/courses")
 public class CourseController {
 
     private final CourseService courseService;
@@ -49,9 +49,5 @@ public class CourseController {
         courseService.deleteCourseById(courseId);
 
     }
-
-
-
-
 
 }
