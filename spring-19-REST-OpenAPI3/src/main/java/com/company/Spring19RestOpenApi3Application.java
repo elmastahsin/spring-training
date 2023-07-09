bfa1b09c236997bffac9fa817919aac24949ba97
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-07-09 11:41:04 +0300

    spring rest openapi

---
+++ spring-19-REST-OpenAPI3/src/main/java/com/company/Spring19RestOpenApi3Application.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class Spring19RestOpenApi3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring19RestOpenApi3Application.class, args);
+    }
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info()
+                .title("OPENAPI Application OpenAPI")
+                .version("v1")
+                .description("OPENAPI application API documentation"))
+                .servers(List.of(new Server().url("https://dev.company.com").description("Dev Environment")));
+    }
+
+}
