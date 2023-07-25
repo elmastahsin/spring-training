bc9c041581e12f9024ce24fba35f3ee85a5824e4
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-07-25 12:11:27 +0300

    spring aop

--- spring-21-AOP/src/main/java/com/company/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/company/aspect/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.company.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,33 @@ public class LoggingAspect {
 
     Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut("execution(* com.company.controller.CourseController.*(..))")
-    private void myPointCut() {}
+//    @Pointcut("execution(* com.company.controller.CourseController.*(..))")
+//    private void myPointCut() {
+//    }
+//
+//    @Before("myPointCut()")
+//    public void log(){
+//        logger.info("Info log ........");
+//    }
+
+
+//    @Before("execution(* com.company.controller.CourseController.*(..))()")
+//    public void log(){
+//        logger.info("Info log ........");
+//    }
+
+    @Pointcut("execution(* com.company.repository.CourseRepository.findById(*))")
+    private void courseRepoFindById() {
+    }
+
+    @Before("courseRepoFindById()")
+    public void BeforeCourseRepoFindById(JoinPoint joinPoint) {
+        logger.info("Before -> Method : {} ,Arguments {},Target :{}"
+                ,joinPoint.getSignature(),joinPoint.getArgs(),joinPoint.getTarget());
+
+    }
+
+
+
+
 }
