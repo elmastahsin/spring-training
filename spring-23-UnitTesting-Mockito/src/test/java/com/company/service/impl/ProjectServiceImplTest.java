b11009c7b42764868862db03085576865b82176d
tahsinelmas <elmas.tahsin@metu.edu.tr>
2023-08-01 15:43:10 +0300

    JUnit mockito

--- spring-23-UnitTesting-Mockito/src/test/java/com/company/service/impl/ProjectServiceImplTest.java
+++ spring-23-UnitTesting-Mockito/src/test/java/com/company/service/impl/ProjectServiceImplTest.java
@@ -6,11 +6,14 @@ import com.company.service.ProjectService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import com.company.mapper.ProjectMapper;
 import com.company.repository.ProjectRepository;
 import com.company.service.ProjectService;
+
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -23,23 +26,37 @@ class ProjectServiceImplTest {
     ProjectRepository projectRepository;
     @Mock
     ProjectMapper projectMapper;
-    @Mock
-    ProjectService projectService;
+    @InjectMocks
+    ProjectServiceImpl projectService;
 
     @Test
-    void getByProjectCodeTest() {
-        when(projectRepository.findByProjectCode(anyString())).thenReturn(new Project());//stubbing
+    void getByProjectCode_Test() {
+        // Given        --      Preparation of the test
+        when(projectRepository.findByProjectCode(anyString())).thenReturn(new Project());   // Stubbing
         when(projectMapper.convertToDto(any(Project.class))).thenReturn(new ProjectDTO());
 
+        // When         --      Execution of the real method
         ProjectDTO projectDTO = projectService.getByProjectCode(anyString());
 
-        InOrder inOrder = inOrder(projectRepository, projectMapper);
+        // Then         --      Verification and assertion
+        InOrder inOrder = inOrder(projectRepository, projectMapper);  // I want to check the order of these 2 Mocks
 
-        inOrder.verify(projectRepository).findByProjectCode(anyString());
+        inOrder.verify(projectRepository).findByProjectCode(anyString());   // We are providing in which order these 2 Mocks should be
         inOrder.verify(projectMapper).convertToDto(any(Project.class));
 
         assertNotNull(projectDTO);
+
+
     }
 
+    @Test
+    void getByProjectCode_ExceptionTest(){
+        when(projectRepository.findByProjectCode("")).thenThrow(new NoSuchElementException("Project not found!"));
+        Throwable throwable = assertThrows(NoSuchElementException.class, () -> projectService.getByProjectCode(""));
 
+        verify(projectRepository).findByProjectCode("");
+        verify(projectMapper, never()).convertToDto(any(Project.class));
+
+        assertEquals("Project not found!", throwable.getMessage());
+    }
 }
---
+++ spring-23-UnitTesting-Mockito/src/test/java/com/company/service/impl/TaskServiceImplTest.java
@@ -0,0 +1,62 @@
+package com.company.service.impl;
+
+import com.company.dto.TaskDTO;
+import com.company.entity.Task;
+import com.company.mapper.TaskMapper;
+import com.company.repository.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceImplTest {
+
+    @Mock
+    TaskRepository taskRepository;
+    @Mock
+    TaskMapper taskMapper;
+
+    @InjectMocks
+    TaskServiceImpl taskService;
+
+
+    @ParameterizedTest
+    @ValueSource(longs = {1L, 2L, 3L})
+    void findById_Test(long id) {
+        //Given(Preparation of the test)
+        Task task = new Task();
+        //When (Action is performed)
+        when(taskRepository.findById(id)).thenReturn(Optional.of(task)); //Optional<Task>
+        when(taskMapper.convertToDto(task)).thenReturn(new TaskDTO());
+        taskService.findById(id);
+
+        //Then (Assertion and Verification)
+        verify(taskRepository).findById(id);
+        verify(taskMapper).convertToDto(task);
+    }
+
+    @Test
+    void findById_BDD_Test() {
+        //Given(Preparation of the test)
+        Task task = new Task();
+        //When (Action is performed)
+        given(taskRepository.findById(anyLong())).willReturn(Optional.of(task)); //Optional<Task>
+        given(taskMapper.convertToDto(task)).willReturn(new TaskDTO());
+
+
+        //Then (Assertion and Verification)
+        verify(taskRepository).findById(anyLong());
+        verify(taskMapper).convertToDto(task);
+    }
+
+}
