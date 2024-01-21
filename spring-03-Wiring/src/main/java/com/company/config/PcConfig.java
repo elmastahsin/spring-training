fc0f140048fd75ee5e39abecd00894c5d84d29dc
tahsinelmas <elmas.tahsin@metu.edu.tr>
2024-01-21 21:06:48 +0300

    DependencyI

--- spring-03-Wiring/src/main/java/com/company/config/PcConfig.java
+++ spring-03-Wiring/src/main/java/com/company/config/PcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PcConfig {
+public class  PcConfig {
     @Bean
     public Dimensions dimensions() {
         return new Dimensions(50, 12, 21);
