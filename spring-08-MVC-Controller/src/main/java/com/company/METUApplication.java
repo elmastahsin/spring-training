67411f2ab8d86223e32f65d84314c62f70ab2176
tahsinelmas <elmas.tahsin@metu.edu.tr>
2024-01-23 11:14:34 +0300

    SpringBootWeb

--- spring-08-MVC-Controller/src/main/java/com/company/METUApplication.java
+++ spring-08-MVC-Controller/src/main/java/com/company/METUApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//SpringBootApplication is a combination of @Configuration, @EnableAutoConfiguration, @ComponentScan
+// @Configuration: tags the class as a source of bean definitions for the application context.
+// @EnableAutoConfiguration: tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings.
+// @ComponentScan: tells Spring to look for other components, configurations, and services in the com/example package, letting it find the controllers.
 //@SpringBootApplication(exclude = {AopAutoConfiguration.class})
 public class METUApplication {
 
